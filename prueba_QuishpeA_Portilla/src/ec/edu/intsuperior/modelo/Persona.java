006ddad171278bdc88ce837e757a31cbc2463f27
Anderson Patricio <quishpe.anderson@intsuperior.edu.ec>
2022-07-12 22:04:13 -0500

    Version de Anderson Quishpe

--- prueba_QuishpeA_Portilla/src/ec/edu/intsuperior/modelo/Persona.java
+++ prueba_QuishpeA_Portilla/src/ec/edu/intsuperior/modelo/Persona.java
@@ -4,11 +4,129 @@
  * and open the template in the editor.
  */
 package ec.edu.intsuperior.modelo;
-
+import java.util.Date;
 /**
  *
  * @author quish
  */
 public class Persona {
+   private String nombre1;
+    private String nombre2;
+    private String ci;
+    private String apellido1;
+    private String apellido2;
+    private String direccion;
+    private String celular;
+    private String correo;
+    private Date fechaNacimiento;
+    private String edad;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre1, String nombre2, String ci, String apellido1, String apellido2, String direccion, String celular, String correo, Date fechaNacimiento, String edad) {
+        this.nombre1 = nombre1;
+        this.nombre2 = nombre2;
+        this.ci = ci;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.direccion = direccion;
+        this.celular = celular;
+        this.correo = correo;
+        this.fechaNacimiento = fechaNacimiento;
+        this.edad = edad;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public void setCi(String ci) {
+        this.ci = ci;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public void setNombre1(String nombre1) {
+        this.nombre1 = nombre1;
+    }
+
+    public void setNombre2(String nombre2) {
+        this.nombre2 = nombre2;
+    }
     
+    public void setEdad (String edad){
+        this.edad = edad;
+    }
+
+    public String getNombre1() {
+        return nombre1;
+    }
+
+    public String getNombre2() {
+        return nombre2;
+    }
+
+    public String getCi() {
+        return ci;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+    
+    public String getEdad(){
+        return edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Los datos del objeto son: \n"
+                + "Cedula: "+getCi()+"\n"
+                + "Apellidos y Nombres: "+getApellido1()+" "+getApellido2()+
+                " "+getNombre1()+" "+getNombre2()+"\n"
+                + "Direccion: "+getDireccion()+"\n"
+                + "Celular: "+getCelular()+"\n"
+                + "Correo: "+getCorreo()+"\n"
+                + "Edad: "+getEdad()+"\n";
+                
+                } 
 }
--- prueba_QuishpeA_Portilla/src/ec/edu/intsuperior/vista/Aplicacion.java
+++ prueba_QuishpeA_Portilla/src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -4,13 +4,24 @@
  * and open the template in the editor.
  */
 package ec.edu.intsuperior.vista;
-
+import javax.swing.JOptionPane;
+import ec.edu.intsuperior.modelo.Persona;
 /**
  *
  * @author quish
  */
 public class Aplicacion {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+       
+        Persona p1= new Persona();
+        p1.setCi(JOptionPane.showInputDialog("Ingresa tu numero de cedula"));
+        p1.setNombre1(JOptionPane.showInputDialog("Ingresa tu primer nombre"));
+        p1.setNombre2(JOptionPane.showInputDialog("Ingresa tu segundo nombre"));
+        p1.setApellido1(JOptionPane.showInputDialog("Ingresa el apellido paterno"));
+        p1.setApellido2(JOptionPane.showInputDialog("Ingresa el apellido materno"));
+        p1.setEdad(JOptionPane.showInputDialog("Ingresa tu edad"));
+        p1.setDireccion(JOptionPane.showInputDialog("Ingrese su Direccion"));
+        JOptionPane.showMessageDialog(null,p1.toString());
         
     }
     
